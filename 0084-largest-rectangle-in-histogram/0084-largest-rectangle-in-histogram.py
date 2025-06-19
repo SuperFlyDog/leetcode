dc99c8a124f98001b30c94e540ad925c026c7b2e
Kang Yin <yinkang.cs@outlook.com>
2025-06-19 18:11:38 +0900

    Time: 216 ms (44.82%), Space: 22.2 MB (65.76%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -4,19 +4,17 @@ class Solution(object):
         :type heights: List[int]
         :rtype: int
         """
-        res = []
+        # 在末尾加一个 0 高度柱子，确保所有柱子都会被处理
+        heights.append(0)
+        stack = [-1]  # 初始化栈，存放的是下标。栈底是哨兵 -1，便于处理宽度
+        max_area = 0
 
-        def split(h):
-            if not h:
-                return
-            if len(h) == 1:
-                res.append(h[0])
-                return
-            min_h = min(h)
-            min_index = h.index(min_h)
-            res.append(len(h) * min_h)
-            split(h[:min_index])       # 左半部分
-            split(h[min_index + 1:])   # 右半部分
+        for i in range(len(heights)):
+            # 当前柱子比栈顶矮，说明栈顶柱子右边界到了，开始出栈
+            while stack[-1] != -1 and heights[i] < heights[stack[-1]]:
+                height = heights[stack.pop()]  # 弹出栈顶元素的高度
+                width = i - stack[-1] - 1       # 当前 i 是右边界，新的栈顶是左边界
+                max_area = max(max_area, height * width)
+            stack.append(i)  # 当前柱子入栈
 
-        split(heights)
-        return max(res)
+        return max_area
