0b80a20b30f7aab099f3607ce45c1ea6266bac4b
Kang Yin <yinkang.cs@outlook.com>
2025-08-29 17:46:39 +0900

    Time: 4 ms (26.12%), Space: 12.7 MB (33.64%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -4,14 +4,10 @@ class Solution(object):
         :type citations: List[int]
         :rtype: int
         """
-        n = len(citations)
+        citations.sort(reverse=True)
         h_index = 0
-        # 枚举所有可能的 h
-        for h in range(n+1):
-            count = 0
-            for paper in citations:
-                if paper >= h:
-                    count += 1
-            if count >= h:
-                h_index = max(h_index, h)
+        for i in range(len(citations)):
+            if citations[i] >= i+1:
+                h_index = i+1
         return h_index
+
